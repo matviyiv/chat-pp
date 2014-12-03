ccf6040b3e21461002a71a745e46e24e0615561b
Andrii Varava <andrii.varava@sethq.com>
2014-12-03 14:35:08 +0200

    added basic functionality for express server

---
+++ server/bin/www.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+var app = require('./../app'),
+    server;
+
+app.set('hostname', process.env.HOSTNAME || 'localhost');
+app.set('port', process.env.PORT || 8080);
+
+
+server = app.listen(app.get('port'), app.get('hostname'), function () {
+    console.log('Express server listening on port ' + server.address().address + ':' + server.address().port);
+});
